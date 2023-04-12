f516fa93d121f8436d89da4b92a6b57dd57c5e83
PrgDominique <dominiquesimbillo10@gmail.com>
2023-04-12 20:15:35 +0800

    init

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react'
+
+type Name = {
+  name: string
+}
 
 const about = () => {
+  const [showData, setShowData] = useState<Name | undefined>()
+
+  const getData = async () => {
+    try {
+      const uri = await fetch('http://localhost:3000/api/hello', {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const response = await uri.json()
+      setShowData(response)
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+
   return (
-    <div>about</div>
+    <div className='h-screen'>
+      <div className='text-center justify-center  '>about</div>
+      {showData && <div>{showData.name}</div>}
+    </div>
   )
 }
 
